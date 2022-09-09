1c3bab80229f4e608ff7fe5e3046a2662cef1207
Adel <56457765+ad3ldev@users.noreply.github.com>
2022-09-10 01:54:19 +0200

    Notifications and intent

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,11 @@
 package com.udacity
 
+import android.app.DownloadManager
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,6 +13,20 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        if (intent.hasExtra("title")) {
+            fileName.text = intent.getStringExtra("title")
+            fileName.setTextColor(Color.BLACK)
+        }
+        if (intent.hasExtra("status")) {
+            if (intent?.getIntExtra("status", -1) == DownloadManager.STATUS_SUCCESSFUL) {
+                status.text = "Success"
+                status.setTextColor(Color.GREEN)
+            } else {
+                status.text = "Failed"
+                status.setTextColor(Color.RED)
+            }
+        }
     }
 
 }
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.udacity
 
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
@@ -32,11 +34,25 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        createChannel(CHANNEL_ID, "Download Channel")
+
         custom_button.setOnClickListener {
             when (radioGroup.checkedRadioButtonId) {
-                R.id.choiceGlide -> download(glideURL)
-                R.id.choiceLoadApp -> download(udacityURL)
-                R.id.choiceRetrofit -> download(retrofitURL)
+                R.id.choiceGlide -> download(
+                    "Glide",
+                    getString(R.string.glide_string),
+                    glideURL
+                )
+                R.id.choiceLoadApp -> download(
+                    "LoadApp",
+                    getString(R.string.load_app_string),
+                    udacityURL
+                )
+                R.id.choiceRetrofit -> download(
+                    "Retrofit",
+                    getString(R.string.retrofit_string),
+                    retrofitURL
+                )
                 else -> Toast.makeText(
                     applicationContext,
                     "Please select the file to download",
@@ -50,24 +66,37 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
             if (downloadID == id) {
-                Log.w("DownloadID = ${downloadID}", id.toString())
+                val query = DownloadManager.Query()
+                val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+                val cursor = downloadManager.query(query)
+                if (cursor.moveToFirst()) {
+                    val success =
+                        cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                    val downloadTitle =
+                        cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_TITLE))
+                    notificationManager.sendNotification(
+                        applicationContext,
+                        downloadTitle,
+                        success
+                    )
+                }
             }
         }
     }
 
 
-    private fun download(url: String) {
+    private fun download(title: String, description: String, url: String) {
         val request =
             DownloadManager.Request(Uri.parse(url))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
+                .setTitle(title)
+                .setDescription(description)
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
         downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+            downloadManager.enqueue(request) // enqueue puts the download request in the queue.
     }
 
     companion object {
@@ -77,7 +106,70 @@ class MainActivity : AppCompatActivity() {
             "https://github.com/bumptech/glide/archive/master.zip"
         private const val retrofitURL =
             "https://github.com/square/retrofit/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
+        private const val CHANNEL_ID = "channelID"
+        private const val NOTIFICATION_ID = 0
+        private const val REQUEST_CODE = 0
+        private const val FLAGS = 0
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+                .apply {
+                    setShowBadge(false)
+                }
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(R.string.notification_description)
+            notificationManager = this.getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun NotificationManager.sendNotification(
+        applicationContext: Context, title: String, status: Int
+    ) {
+        val contentIntent = Intent(applicationContext, DetailActivity::class.java)
+        pendingIntent = PendingIntent.getActivity(
+            applicationContext, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        contentIntent.putExtra("title", title)
+        contentIntent.putExtra("status", status)
+        val downloadedPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            REQUEST_CODE,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        action = NotificationCompat.Action(
+            R.drawable.ic_assistant_black_24dp,
+            applicationContext.getString(R.string.notification_title),
+            downloadedPendingIntent
+        )
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        ).setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(applicationContext.getString(R.string.notification_title))
+            .setContentText(applicationContext.getString(R.string.notification_description))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .addAction(
+                action
+            )
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        notify(NOTIFICATION_ID, builder.build())
+
     }
 }
+
+
 
