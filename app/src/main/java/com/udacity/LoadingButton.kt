2bd559a27297976b1dc186c343b7846496d6798e
Adel <56457765+ad3ldev@users.noreply.github.com>
2022-09-09 18:05:20 +0200

    text on design

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,7 +2,7 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.withStyledAttributes
@@ -22,7 +22,13 @@ class LoadingButton @JvmOverloads constructor(
 
     private var normalColor = 0
     private var loadingColor = 0
+    private var buttonString: String = ""
 
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 60.0f
+    }
 
     init {
         isClickable = true
@@ -30,12 +36,22 @@ class LoadingButton @JvmOverloads constructor(
             normalColor = getColor(R.styleable.LoadingButton_normalColor, 0)
             loadingColor = getColor(R.styleable.LoadingButton_loadingColor, 0)
         }
+        buttonString = resources.getString(R.string.button_name)
     }
 
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawColor(normalColor)
+        val rect = Rect()
+        paint.color = normalColor
+        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+        paint.color = Color.WHITE
+        canvas?.drawText(
+            buttonString,
+            widthSize / 2f,
+            heightSize.toFloat() - paint.textSize ,
+            paint
+        )
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
